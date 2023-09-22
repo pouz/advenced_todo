31a3cdbb580ad813c3b8729748117fdac16d2527
pouz <whitewyrm84@gmail.com>
2023-09-22 14:35:06 +0900

    fixing window size on Windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
+
+const double windowWidth = 400;
+const double windowHeight = 800;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('ADVENCED_TODO');
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Container();
