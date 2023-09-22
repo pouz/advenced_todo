5507eeffb2bfd19a6000c09476351e7b5ce18d85
pouz <whitewyrm84@gmail.com>
2023-09-22 17:02:49 +0900

    add foldable_todos.dart

---
+++ lib/screen/show_list_screen/components/foldable_todos.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FoldableTodos extends StatefulWidget {
+  const FoldableTodos({super.key});
+
+  @override
+  State<FoldableTodos> createState() => _FoldableTodosState();
+}
+
+class _FoldableTodosState extends State<FoldableTodos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Row(
+          children: [],
+        ),
+      ],
+    );
+  }
+}
