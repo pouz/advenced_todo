53cd71da7f658169dd1a690511ffda7cdb3e61c3
pouz <whitewyrm84@gmail.com>
2023-09-22 13:23:00 +0900

    add screens, utils

---
+++ lib/screen/show_list_screen/show_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowListScreen extends StatefulWidget {
+  const ShowListScreen({super.key});
+
+  @override
+  State<ShowListScreen> createState() => _ShowListScreenState();
+}
+
+class _ShowListScreenState extends State<ShowListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
